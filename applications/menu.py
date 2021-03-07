c1122267dab8c0d3c6996f83ef0152938249bdea
Pascal Schärli <pas.schaerli@sunrise.ch>
2021-03-07 21:50:06 +0100

    Menus can open applications

--- applications/menu.py
+++ applications/menu.py
@@ -4,39 +4,31 @@ import colorsys
 import hashlib
 
 class Menu(core.Application):
-    def __init__(self, applications, *args, speed=0.01, **kwargs):
+    def __init__(self, applications, *args, speed=4, **kwargs):
         super().__init__(*args, **kwargs)
         self.applications = applications
         self.choice_index = 0
         self.choice_current = 0
         self.speed = speed
-        self.child = None
 
-        self.io.controller.right.subscribe(self.nextChoice)
-        self.io.controller.left.subscribe(self.previousChoice)
-            
-    @core.controllerInput
-    def nextChoice(self, value):
-        if value:
+    def update(self, io, delta):
+        if io.controller.right.get_fresh_value():
             self.choice_index = (self.choice_index + 1) % len(self.applications)
-
-    @core.controllerInput
-    def previousChoice(self, value):
-        if value:
+        if io.controller.left.get_fresh_value():
             self.choice_index = (self.choice_index - 1) % len(self.applications)
-
-    @core.controllerInput
-    def choose(self, value):
-        pass
-    
-    def update(self):
+        if io.controller.a.get_fresh_value():
+            io.openApplication(self.applications[self.choice_index])
+        
         diff = self.choice_index - self.choice_current
+
         if diff > len(self.applications)/2:
             diff -= len(self.applications)
         elif diff < -len(self.applications)/2:
             diff += len(self.applications)
-        if abs(diff) > self.speed:
-            diff = self.speed if diff > 0 else -self.speed
+
+        max_diff = delta * self.speed
+        if abs(diff) > max_diff:
+            diff = max_diff if diff > 0 else -max_diff
         
         self.choice_current += diff
         
@@ -60,5 +52,9 @@ class Menu(core.Application):
         hue = int(hashlib.md5(self.applications[choice_2].name.encode()).hexdigest(),16)%255
         color = tuple(map(lambda x:int(x*255),colorsys.hsv_to_rgb(hue/255, 1, 1)))
 
-        bitmaputils.applyBitmap(bmp, self.io.display, (int(-11 + progression * -12), self.io.display.height//2-2), color0=(0,0,0), color1=color)
-        self.io.display.refresh()
+        for x in range(io.display.width):
+            for y in range(io.display.height):
+                io.display.update(x,y,(0,0,0))
+
+        bitmaputils.applyBitmap(bmp, io.display, (int(-11 + progression * -12), io.display.height//2-2), color0=(0,0,0), color1=color)
+        io.display.refresh()
--- examples/menu_animations.py
+++ examples/menu_animations.py
@@ -3,20 +3,24 @@ import random
 import time
 
 from IO.pygame import PygameIOManager
-from applications.animations import BeatAnimation, AnimationCycler
+from applications.animations import BeatAnimation
+from applications.menu import Menu
 
 if __name__ == "__main__":
     animations = [
         BeatAnimation(
             "resources/animations/shuffle1.npy",
+            name="Shuffle",
             beats_per_loop=1),
         BeatAnimation(
             "resources/animations/shuffle2-2.npy",
+            name="Charleston",
             beats_per_loop=2),
         BeatAnimation(
             "resources/animations/shuffle3.npy",
+            name="Little Big",
             beats_per_loop=2)
     ]
 
     with PygameIOManager() as ioManager:
-        ioManager.run(AnimationCycler(animations))
+        ioManager.run(Menu(animations))
