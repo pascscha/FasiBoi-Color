6366a0f13e77459d6222b4da61a4e17f6b8002a6
Pascal Schärli <pas.schaerli@sunrise.ch>
2021-03-09 20:59:20 +0100

    Documentation on helper functions

--- helpers/bitmaputils.py
+++ helpers/bitmaputils.py
@@ -2,7 +2,7 @@ def applyBitmap(bmp, display, loc, bg_color=None, fg_color=(255, 255, 255)):
     """Applies a bitmap to a display. Cuts away any parts that are out of bounds for the display
 
     Args:
-        bmp (2x2 boolean array): The bitmap to apply
+        bmp (2D boolean array): The bitmap to apply
         display (IO.core.Display): The display to apply the bitmap to 
         loc ((int, int)): The top left coordinates where the bitmap should be applied
         color0 ((int, int, int), optional): The color of the background. Defaults to None, which leaves it transparent
--- helpers/textutils.py
+++ helpers/textutils.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 def getCharBitmap(char):
+    """Gets the bitmap for a single char
+
+    Args:
+        char (chr): The character to get the bitmap from
+
+    Returns:
+        2D boolean array: A 3x5 bitmap representing the given character
+    """
     file_path = "resources/sprites/letters.npy"
     letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?:.><="
     char = char.upper()
@@ -11,6 +19,14 @@ def getCharBitmap(char):
 
 
 def getTextBitmap(text):
+    """Gets a bitmap of a string. Distance between two characters is 1 pixel
+
+    Args:
+        text (str): The string to convert to a bitmap
+
+    Returns:
+        2D boolean array: A bitmap representing the given string
+    """
     out = np.zeros((5, 4*len(text) - 1), np.bool)
     for i, char in enumerate(text):
         out[:, i*4:i*4+3] = getCharBitmap(char)
