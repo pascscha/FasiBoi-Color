8186268b9221f198dfab1c177dbe43e8a7ea9721
Pascal Schärli <github@pascscha.ch>
2021-03-22 09:30:07 +0100

    added imports to video example

--- examples/video.py
+++ examples/video.py
@@ -1,3 +1,6 @@
+from IO.pygame import PygameIOManager
+from applications.animations import VideoPlayer
+
 if __name__ == "__main__":
     with PygameIOManager() as ioManager:
         ioManager.run(VideoPlayer("resources/videos/music.mp4"))
