f135bcd7c3383367d6b11a454e2ae64f20fc6b5a
Pascal Schärli <github@pascscha.ch>
2021-03-26 12:27:04 +0100

    Super ugly but working 2048

---
+++ examples/2048.py
@@ -0,0 +1,6 @@
+from IO.pygame import PygameIOManager
+from applications.games.g2048 import G2048
+
+if __name__ == "__main__":
+    with PygameIOManager() as ioManager:
+        ioManager.run(G2048())
