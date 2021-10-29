73d29cea3734bf52b3a4314c706ea73699b47ffd
Pascal Schärli <github@pascscha.ch>
2021-10-29 14:52:22 +0200

    More efficient color test screen

--- applications/colors.py
+++ applications/colors.py
@@ -13,3 +13,5 @@ class Colors(core.Application):
                 value = (1+vert_box) / (io.display.height//3)
                 saturation = 1 - (y % 3)/3
                 io.display.update(x, y, Color.from_hsv(hue, saturation, value))
+        
+        self.sleep([core.ButtonPressWaker(io.controller.button_menu)])
