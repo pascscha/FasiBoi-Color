50d07c34d6459e9e39f489bac3bd5b1b9252dc73
Pascal Schärli <github@pascscha.ch>
2021-11-05 14:24:05 +0100

    Draw images to screen

--- helpers/bitmaputils.py
+++ helpers/bitmaputils.py
@@ -1,6 +1,18 @@
 import math
 import numpy as np
 
+def cut(img_w, img_h, disp_w, disp_h, x, y):
+    img_left = max(0, -x)
+    img_top = max(0, -y)
+    img_right =  min(img_w, disp_w - x)
+    img_bottom = min(img_h, disp_h - y)
+
+    dsp_left = max(0, x)
+    dsp_top = max(0, y)
+    dsp_right = dsp_left + img_right - img_left
+    dsp_bottom = dsp_top + img_bottom - img_top
+
+    return (img_left, img_top, img_right, img_bottom), (dsp_left, dsp_top, dsp_right, dsp_bottom)
 
 def apply_bitmap(bmp, display, loc, bg_color=None, fg_color=(255, 255, 255)):
     """Applies a bitmap to a display. Cuts away any parts that are out of bounds for the display
@@ -16,29 +28,32 @@ def apply_bitmap(bmp, display, loc, bg_color=None, fg_color=(255, 255, 255)):
     x, y = loc
     h, w = bmp.shape
 
-    
-    bmp_left = max(0, -x)
-    bmp_top = max(0, -y)
-    bmp_right =  min(w, display.width - x)
-    bmp_bottom = min(h, display.height - y)
-
-    dsp_left = max(0, x)    
-    dsp_top = max(0, y)    
-    dsp_right = dsp_left + bmp_right - bmp_left
-    dsp_bottom = dsp_top + bmp_bottom - bmp_top
-
+    (bmp_left, bmp_top, bmp_right, bmp_bottom), (dsp_left, dsp_top, dsp_right, dsp_bottom) = cut(w, h, display.width, display.height, x, y)
     
     cut_dsp = display.pixels[dsp_left:dsp_right, dsp_top:dsp_bottom]
     
-    x, y, _ = cut_dsp.shape
-
-    if x*y > 0:
+    cw, ch, _ = cut_dsp.shape
+    if cw*ch > 0:
         cut_bmp = bmp.T[bmp_left:bmp_right, bmp_top:bmp_bottom]
 
         if fg_color is not None:
             cut_dsp[np.where(cut_bmp)] = fg_color
         if bg_color is not None:
             cut_dsp[np.where(1-cut_bmp)] = bg_color
+
+def apply_image(img, display, loc):
+    x, y = loc
+    h, w = img.shape[:2]
+
+    (img_left, img_top, img_right, img_bottom), (dsp_left, dsp_top, dsp_right, dsp_bottom) = cut(w, h, display.width, display.height, x, y)
+    
+    if (dsp_right-dsp_left)*(dsp_bottom-dsp_top) > 0:
+        cut_img = img.transpose(1, 0, 2)[img_left:img_right, img_top:img_bottom]
+
+        if cut_img.shape[2] == 3:
+            display.pixels[dsp_left:dsp_right, dsp_top:dsp_bottom] = cut_img
+        else:
+            display.pixels[dsp_left:dsp_right, dsp_top:dsp_bottom] = cut_img[:,:,:3]
 
 
 def get_color(image, loc, default=np.array((0, 0, 0))):
