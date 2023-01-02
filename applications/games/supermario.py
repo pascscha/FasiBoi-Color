292e23fb05f33c7bdf1b3960dc97cd68f99516a2
Pascal Schärli <github@pascscha.ch>
2023-01-02 15:51:43 +0100

    Better gravity

--- applications/games/supermario.py
+++ applications/games/supermario.py
@@ -5,89 +5,67 @@ import cv2
 import numpy as np
 
 
-# class MovingEntity:
-#     Y_DELTA=0.1
-
-#     def __init__(self, pos, parent, size=(1,1), fall=False, vel=(2, 0)):
-#         self.pos = pos
-#         self.parent = parent
-#         self.fall = fall
-#         self.vel = list(vel)
-#         self.size = size
-
-#     def is_colliding(self, x, y):
-#         for dx in range(self.size[0]):
-#             for dy in range(self.size[1]):
-#                 if self.parent.is_solid(x + dy, y+dy):
-#                     return True
-#         return False
-
-#     def update_physics(self, delta):
-#         # on_floor = self.parent.is_solid(self.pos[0], self.pos[1] + 1)
-#         # if self.velocity[1] > 0 and on_floor:
-#         #     self.velocity[1] = 0
-#         self.vel[1] += self.parent.GRAVITY * delta
-
-#         new_x = self.pos[0] + self.vel[0] * delta
-#         new_y = self.pos[1] + self.vel[1] * delta
-        
-#         if self.is_colliding(new_x, pos[1]):
-#             self.vel[0] = 0
-#             self.new_x = pos[0]
-        
-#         if self.is_colliding(new_x, new_y):
-#             self.vel[1] = 0
-#             self.new_y = pos[1]
-        
-
-        # if not self.is_solid(
-        #     self.mario_pos[0] + self.mario_vel[0] * delta, self.mario_pos[1]
-        # ) and not self.is_solid(
-        #     self.mario_pos[0] + self.mario_vel[0] * delta, self.mario_pos[1] - 1
-        # ):
-        #     self.mario_pos[0] += self.mario_vel[0] * delta
-
-        # if not self.is_solid(
-        #     self.mario_pos[0], self.mario_pos[1] + self.mario_vel[1] * delta
-        # ) and not self.is_solid(
-        #     self.mario_pos[0], self.mario_pos[1] + self.mario_vel[1] * delta - 1
-        # ):
-        #     self.mario_pos[1] += self.mario_vel[1] * delta
-        # else:
-        #     ix = int(self.mario_pos[0])
-        #     iy = int(self.mario_pos[1] + self.mario_vel[1] * delta - 1)
-        #     if self.is_box(ix, iy):
-        #         self.boxes.remove((ix, iy))
-        #     self.mario_vel[1] = 0
-
-        # if not on_floor:
-        #     self.mario_vel[1] += self.GRAVITY * delta
-
-
 class MovingEntity:
-    def __init__(self, pos, parent, fall=False, velocity=(2, 0)):
+    Y_DELTA = 0.1
+
+    def __init__(self, parent, pos, size=(1, 1), fall=False, vel=(0, 0)):
         self.pos = pos
         self.parent = parent
         self.fall = fall
-        self.velocity = list(velocity)
+        self.vel = list(vel)
+        self.size = size
+
+    def is_colliding(self, x, y):
+        for dx in range(self.size[0]):
+            for dy in range(self.size[1]):
+                if self.parent.is_solid(x + dx, y - dy):
+                    return True
+        return False
+
+    def on_floor(self):
+        for dx in range(self.size[0]):
+            if self.parent.is_solid(self.pos[0] + dx, self.pos[1] + 1):
+                return True
+        return False
 
     def update(self, delta):
-        new_x = self.pos[0] + self.velocity[0] * delta
-
-        new_y = self.pos[1]
-
-        if (
-            self.fall or self.parent.is_solid(new_x - 0.5, new_y + 1.5)
-        ) and not self.parent.is_solid(new_x - 0.5, new_y + 0.5):
-
-            if self.fall and not self.parent.is_solid(new_x - 0.5, new_y + 1.5):
-                self.velocity[1] += self.parent.GRAVITY * delta
-                new_y = self.pos[1] + self.velocity[1] * delta
-            else:
-                new_y = int(new_y)
-            self.pos = (new_x, new_y)
+        if self.vel[1] > 0 and self.on_floor():
+            self.vel[1] = 0
         else:
-            self.velocity[0] *= -1
+            self.vel[1] += self.parent.GRAVITY * delta
+
+        new_x = self.pos[0] + self.vel[0] * delta
+        new_y = self.pos[1] + self.vel[1] * delta
+
+        if self.is_colliding(self.pos[0], new_y):
+            self.vel[1] = 0
+            new_y = self.pos[1]
+
+        if self.is_colliding(new_x, new_y):
+            self.vel[0] = 0
+            new_x = self.pos[0]
+
+        self.pos = [new_x, new_y]
+
+
+class Goomba(MovingEntity):
+    def __init__(self, *args, speed=2, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.falling = not self.on_floor()
+        self.speed = speed
+        self.vel[0] = self.speed
+
+    def update(self, delta):
+        prev_pos = self.pos
+        super().update(delta)
+        if not self.falling and not self.on_floor():
+            self.pos = prev_pos
+            self.speed *= -1
+            self.vel[0] = self.speed
+            self.vel[1] = 0
+        elif self.speed != 0 and self.vel[0] == 0:
+            self.speed *= -1
+            self.vel[0] = self.speed
 
 
 class Supermario(core.Game):
@@ -109,24 +87,23 @@ class Supermario(core.Game):
     def load_level(self, level):
         level_data = cv2.imread(f"resources/images/supermario/{level}.png")
         self.floor_bmp = np.all(level_data == self.FLOOR_COLOR, axis=2)
+        self.boxes = []
+
         self.goombas = [
-            MovingEntity((x, y), self)
+            Goomba(self, (x, y))
             for y, x in zip(*np.where(np.all(level_data == self.GOOMBA_COLOR, axis=2)))
         ]
-        self.boxes = [
-            (x - 1, y)
-            for y, x in zip(
-                *np.where(np.all(level_data == tuple(reversed(self.BOX_COLOR)), axis=2))
-            )
-        ]
-        print(self.boxes)
-        print(level_data)
+        # self.boxes = [
+        #     (x - 1, y)
+        #     for y, x in zip(
+        #         *np.where(np.all(level_data == tuple(reversed(self.BOX_COLOR)), axis=2))
+        #     )
+        # ]
+        self.mario_entity = MovingEntity(self, (4.5, 12.5), size=(1, 2))
 
     def reset(self, io):
         self.level = 0
         self.load_level(self.level)
-        self.mario_pos = [4.5, 12.5]
-        self.mario_vel = [0, 0]
 
     def is_box(self, x, y):
         ix = int(x)
@@ -142,63 +119,37 @@ class Supermario(core.Game):
             return False
 
     def update_physics(self, delta):
-        on_floor = self.is_solid(self.mario_pos[0], self.mario_pos[1] + 1)
-
-        if self.mario_vel[1] > 0 and on_floor:
-            self.mario_vel[1] = 0
-
-        if not self.is_solid(
-            self.mario_pos[0] + self.mario_vel[0] * delta, self.mario_pos[1]
-        ) and not self.is_solid(
-            self.mario_pos[0] + self.mario_vel[0] * delta, self.mario_pos[1] - 1
-        ):
-            self.mario_pos[0] += self.mario_vel[0] * delta
-
-        if not self.is_solid(
-            self.mario_pos[0], self.mario_pos[1] + self.mario_vel[1] * delta
-        ) and not self.is_solid(
-            self.mario_pos[0], self.mario_pos[1] + self.mario_vel[1] * delta - 1
-        ):
-            self.mario_pos[1] += self.mario_vel[1] * delta
-        else:
-            ix = int(self.mario_pos[0])
-            iy = int(self.mario_pos[1] + self.mario_vel[1] * delta - 1)
-            if self.is_box(ix, iy):
-                self.boxes.remove((ix, iy))
-            self.mario_vel[1] = 0
-
-        if not on_floor:
-            self.mario_vel[1] += self.GRAVITY * delta
+        self.mario_entity.update(delta)
 
     def _update_midgame(self, io, delta):
         if io.controller.button_up.get_fresh_value():
-            if self.is_solid(self.mario_pos[0], self.mario_pos[1] + 1):
-                self.mario_vel[1] = -0.35 * self.GRAVITY
+            if self.is_solid(self.mario_entity.pos[0], self.mario_entity.pos[1] + 1):
+                self.mario_entity.vel[1] = -0.4 * self.GRAVITY
 
         if io.controller.button_left.get_value():
-            self.mario_vel[0] = -self.SPEED
+            self.mario_entity.vel[0] = -self.SPEED
         elif io.controller.button_right.get_value():
-            self.mario_vel[0] = self.SPEED
+            self.mario_entity.vel[0] = self.SPEED
         else:
-            self.mario_vel[0] = 0
+            self.mario_entity.vel[0] = 0
 
         for goomba in self.goombas:
             goomba.update(delta)
 
         self.update_physics(delta)
-        if self.mario_pos[1] > io.display.height + 2:
+        if self.mario_entity.pos[1] > io.display.height + 2:
             self.state = self.GAME_OVER
 
         kill = []
         for i, goomba in enumerate(self.goombas):
             if (
-                abs(self.mario_pos[0] - goomba.pos[0]) < 0.9
-                and abs(self.mario_pos[1] - goomba.pos[1]) < 1
+                abs(self.mario_entity.pos[0] - goomba.pos[0]) < 0.9
+                and abs(self.mario_entity.pos[1] - goomba.pos[1]) < 1
             ):
-                if self.mario_vel[1] > 0:
+                if self.mario_entity.vel[1] > 0.1:
                     kill.append(i)
                 else:
-                    self.mario_vel[1] = -0.35 * self.GRAVITY
+                    self.mario_entity.vel[1] = -0.35 * self.GRAVITY
                     self.state = self.GAME_OVER
         for k in reversed(kill):
             self.goombas.pop(k)
@@ -211,7 +162,7 @@ class Supermario(core.Game):
 
     def draw_goombas(self, io, delta):
         for goomba in self.goombas:
-            ix = int(4 + goomba.pos[0] - self.mario_pos[0])
+            ix = int(4.5 + goomba.pos[0] - self.mario_entity.pos[0])
             iy = int(goomba.pos[1])
 
             if 0 <= ix < io.display.width:
@@ -220,7 +171,7 @@ class Supermario(core.Game):
 
     def draw_boxes(self, io, delta):
         for x, y in self.boxes:
-            ix = int(5 + x - self.mario_pos[0])
+            ix = int(5 + x - self.mario_entity.pos[0])
             iy = int(y)
 
             if 0 <= ix < io.display.width:
@@ -228,7 +179,7 @@ class Supermario(core.Game):
                     io.display.update(ix, iy, self.BOX_COLOR)
 
     def draw_mario(self, io, delta):
-        iy = int(self.mario_pos[1])
+        iy = int(self.mario_entity.pos[1])
 
         if 0 <= iy < io.display.height:
             io.display.update(4, iy, self.MARIO_TROUSERS)
@@ -239,7 +190,7 @@ class Supermario(core.Game):
         bitmaputils.apply_bitmap(
             self.floor_bmp,
             io.display,
-            (4 - int(self.mario_pos[0]), 0),
+            (4 - int(self.mario_entity.pos[0]), 0),
             fg_color=self.FLOOR_COLOR,
         )
 
@@ -248,9 +199,9 @@ class Supermario(core.Game):
             self.gameover_scroll = animations.AnimatedValue(0, speed=0.3)
             self.gameover_scroll.set_value(1)
 
-        self.mario_vel[0] = 0
-        self.mario_vel[1] += self.GRAVITY * delta
-        self.mario_pos[1] += self.mario_vel[1] * delta
+        self.mario_entity.vel[0] = 0
+        self.mario_entity.vel[1] += self.GRAVITY * delta
+        self.mario_entity.pos[1] += self.mario_entity.vel[1] * delta
 
         io.display.fill((0, 0, 0))
         self.draw_floor(io, delta)
