9069996ee6b6f707f08f16e0a19356656eba618b
Pascal Schärli <github@pascscha.ch>
2023-02-14 09:56:26 +0100

    Run examples on 10FPS

--- examples/milkdrop.py
+++ examples/milkdrop.py
@@ -3,6 +3,6 @@ from applications.milkdrop import Milkdrop
 
 if __name__ == "__main__":
     with PygameIOManager(
-        bg_path=None, screen_pos=(0, 0), screen_size=(500, 750), record_path="milkdrop.mp4"
+        bg_path=None, fps=10, screen_pos=(0, 0), screen_size=(500, 750), record_path="milkdrop.mp4"
     ) as ioManager:
         ioManager.run(Milkdrop())
--- examples/pong.py
+++ examples/pong.py
@@ -2,5 +2,5 @@ from IO.gui import PygameIOManager
 from applications.games.pong import Pong
 
 if __name__ == "__main__":
-    with PygameIOManager() as ioManager:
+    with PygameIOManager(fps=10) as ioManager:
         ioManager.run(Pong())
